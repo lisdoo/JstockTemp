88a44f3a7bce7f0e8c9be98c4479dad82939527a
lisdoo <lisdoo@msn.com>
2021-01-12 09:40:25 +0800

    , jrr.statue

--- sql/jstock.sql
+++ sql/jstock.sql
@@ -1,5 +1,27 @@
 select b.code, b.statusa, b.base_prise, b.statusb, b.`position` ,b.position_prise, jrr.quote_time , jrr.status , jrr.conform , jrr.price from 
-(select a.code, a.statusa, a.base_prise, a.statusb, jr.id, jr.`position` , jr.position_prise from
-(select j.code , j.status as statusa , jr.base_prise , jr.status as statusb , jr.id from jstock j left join jstock_range jr on j.id = jr.jstock_id where code = '002180' and jstock_strategy_id = 9) as a
-left join jstock_range jr on a.id = jr.parent_id) as b
-left join jstock_range_record jrr on b.id = jrr.jstock_range_id ;
+(
+  select a.code, a.statusa, a.base_prise, a.statusb, jr.id, jr.`position` , jr.position_prise from
+  (
+    select j.code , j.status as statusa , jr.base_prise , jr.status as statusb , jr.id from jstock j left join jstock_range jr on j.id = jr.jstock_id where code = '002180' and jstock_strategy_id = 9
+  ) as a
+  left join jstock_range jr on a.id = jr.parent_id
+) as b
+left join jstock_range_record jrr on b.id = jrr.jstock_range_id 
+-- order by quote_time 
+order by `position` desc 
+;
+
+select jrr.price , jrr.status , jrr.quote_time , jrr.volume, jrr.amount from jstock_range_record jrr where id in 
+(
+	select jrr.id from 
+	(
+	  select a.code, a.statusa, a.base_prise, a.statusb, jr.id, jr.`position` , jr.position_prise from
+	  (
+	    select j.code , j.status as statusa , jr.base_prise , jr.status as statusb , jr.id from jstock j left join jstock_range jr on j.id = jr.jstock_id where code = '002180' and jstock_strategy_id = 9
+	  ) as a
+	  left join jstock_range jr on a.id = jr.parent_id
+	) as b
+	left join jstock_range_record jrr on b.id = jrr.jstock_range_id 
+)
+order by jrr.quote_time;
+;
